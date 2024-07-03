2425811092e90721625e254bc3265296bcafdeba
Dora <134863831+Thunder-gg77@users.noreply.github.com>
2024-07-03 16:04:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Reg from './components/Reg'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import First from './components/First'
+import Tab from './components/Tab'
+import Counter from './components/Counter'
+import New from './components/New'
+import Formbasic from './components/Formbasic'
+import Apiget from './components/Apiget'
+import GriD from './components/Grid'
+
+
+function App() {
+ 
+
+  return (
+    <>
+    
+
+    <Navbar/>
+    <Routes>
+      <Route path='/a'element={<First/>}/>
+      <Route path='/r'element={<Reg data={{Name:"jiya",age:12,place:"ERK"}}/>}/>
+      <Route path='/t'element={<Tab/>}/>
+      <Route path='/g'element={<Counter/>}/>
+      <Route path='/f'element={<New/>}/>
+      <Route path='/i'element={<Formbasic />}/>
+      <Route path='/j'element={<Apiget />}/>
+      <Route path='/x'element={<GriD />}/>
+    </Routes> 
+    </>
+
+  )
+}
+
+export default App
